99a70a118780348d05f9d6c2bcbe3a40c6d9f48f
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-03-23 23:21:16 +0900

    feat: RestTemplate 사용 뼈대 구축

---
+++ back-end/src/main/java/com/example/backend/api/global/infrastructure/RestTemplateRequester.java
@@ -0,0 +1,9 @@
+package com.example.backend.api.global.infrastructure;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestTemplateRequester {
+
+    private static final String MARKETING_BASE_URL = "http://openapi.seoul.go.kr:8088/";
+}
