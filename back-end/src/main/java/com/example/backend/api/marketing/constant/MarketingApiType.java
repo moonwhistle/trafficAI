a2b32af3b21c6118941dbc751964b63095e13e2b
Hheojiwon <0618amy@naver.com>
2025-03-28 12:58:19 +0900

    feat: 오피스텔 api type 추가

--- back-end/src/main/java/com/example/backend/api/marketing/constant/MarketingApiType.java
+++ back-end/src/main/java/com/example/backend/api/marketing/constant/MarketingApiType.java
@@ -13,7 +13,12 @@ public enum MarketingApiType {
     // 서울시 상권분석서비스(추정매출-상권)
     ESTIMATED_SALES_MARKETING_AREA("VwsmTrdarSelngQq"),
     // 서울시 행정동별 대중교통 총 승차 승객수
-    PASSENGER_MARKETING_AREA("tpssPassengerCnt");
+    PASSENGER_MARKETING_AREA("tpssPassengerCnt"),
+    // 오피스텔 시가 정보_1
+    OFFICETEL_VALUATION_1("4668bee5-fbfd-41b1-a521-c839c3e01615_201909281433"),
+    // 오피스텔 시가 정보_2
+    OFFICETEL_VALUATION_2("45bfecd8-456a-430d-8a59-bd3d1145835e");
+
 
     private final String type;
 
